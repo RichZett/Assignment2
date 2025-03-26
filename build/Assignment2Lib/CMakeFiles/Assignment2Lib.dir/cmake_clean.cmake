20af2921f7b9e4503469ebe0ace4963cd21092dd
RichZett <rz_13@live.se>
2025-03-26 14:01:37 +0000

    CMake created

---
+++ build/Assignment2Lib/CMakeFiles/Assignment2Lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment2Lib.dir/cmdproc.c.o"
+  "CMakeFiles/Assignment2Lib.dir/cmdproc.c.o.d"
+  "libAssignment2Lib.a"
+  "libAssignment2Lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment2Lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Assignment2Lib/CMakeFiles/Assignment2Lib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Assignment2Lib.
+# This may be replaced when dependencies are built.
